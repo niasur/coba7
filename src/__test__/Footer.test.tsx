23b6cdbce95a509ac2e4e09d6c9d5601cae8cbf7
niasur <rusitainnya@gmail.com>
2024-12-02 16:50:49 +0700

    Update Footer.test.tsx

--- src/__test__/Footer.test.tsx
+++ src/__test__/Footer.test.tsx
@@ -1,22 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Footer from '../components/Footer'; // Pastikan path ini sesuai dengan lokasi file Footer
+import Footer from '../components/Footer'; 
 
 describe('Footer Component', () => {
   it('renders the Footer with the correct text', () => {
     render(<Footer />);
 
-    // Dapatkan tahun saat ini
     const currentYear = new Date().getFullYear();
 
-    // Periksa apakah teks footer berisi copyright dengan tahun saat ini
     expect(screen.getByText(`© ${currentYear} My Portfolio @niaasur. All rights reserved.`)).toBeInTheDocument();
   });
 
   it('has the correct class for styling', () => {
     const { container } = render(<Footer />);
 
-    // Periksa apakah elemen footer memiliki kelas CSS yang sesuai
     expect(container.firstChild).toHaveClass('bg-gray-800');
     expect(container.firstChild).toHaveClass('text-white');
     expect(container.firstChild).toHaveClass('p-4');
